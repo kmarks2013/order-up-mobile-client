52e76ddd7c9692e2652539639ac377d832e29614
kmarks2013 <kmarks2013@gmail.com>
2020-04-28 22:31:50 -0400

    adds src folder and home screen"

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, Stylesheet } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Test</Text>
+        </View>
+    )
+}
+
+const styles = Stylesheet.create({
+
+})
+
+export default HomeScreen
