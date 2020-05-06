cb4f53477109c643adff671a2100a1ab8ef2a0b6
kmarks2013 <kmarks2013@gmail.com>
2020-05-06 18:14:49 -0400

    imports searchbar too HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import SearchBar from '../components/SearchBar'
 
 const HomeScreen = () => {
     return (
         <View>
+            <SearchBar/>
             <Text style={styles.homeFont}>Test</Text>
         </View>
     )
