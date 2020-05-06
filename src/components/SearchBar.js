269c1bbc0b95de7505bdb37b861eec55a26756c6
kmarks2013 <kmarks2013@gmail.com>
2020-05-06 18:14:17 -0400

    adds static text for SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 const SearchBar = () => {
     return (
         <View>
-            <Text></Text>
+            <Text>Search Bar</Text>
         </View>
     )
 }
