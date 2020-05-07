cdb807f1ec5b2f019892537b9900125e9095440b
kmarks2013 <kmarks2013@gmail.com>
2020-05-06 21:51:14 -0400

    updates name of background style

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ import { AntDesign } from '@expo/vector-icons';
 
 const SearchBar = () => {
     return (
-        <View style={styles.background}>
+        <View style={styles.backgroundStyle}>
             <AntDesign name="search1" size={30} color="black" />
             <TextInput />
         </View>
@@ -12,9 +12,9 @@ const SearchBar = () => {
 }
 
 const styles= StyleSheet.create({
-    background: {
-        backgroundColor: '#B2B2B2',
-        // backgroundColor: '#F0EEEE',
+    backgroundStyle: {
+        // backgroundColor: '#B2B2B2',
+        backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
         marginHorizontal: 15
