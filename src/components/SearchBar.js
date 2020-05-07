46e6575ab78600398ba895afe755d5176d87ea1f
kmarks2013 <kmarks2013@gmail.com>
2020-05-06 21:32:54 -0400

    adds ico to searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
 
 const SearchBar = () => {
     return (
         <View style={styles.background}>
+            <AntDesign name="search1" size={24} color="black" />
             <Text>Search Bar</Text>
         </View>
     )
