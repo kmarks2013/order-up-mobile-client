c4749f5dbebe2021b581375d4f5b78cc9601969c
kmarks2013 <kmarks2013@gmail.com>
2020-05-15 11:50:00 -0400

    adds space back now that error message works

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers:{
         Authorization:
-        'BearerWYScSl9qEUwysrj6BNDJgd2BLQFwnTtnGoYsprwca03Heq6gdkaAjZ-M1z4srmKZ4GF9DGOuXwjfokf0H_XipeKvLoQq-QjaAgvE9zQkczomk0RDT9Tg-JerlJeoXnYx'
+        'Bearer WYScSl9qEUwysrj6BNDJgd2BLQFwnTtnGoYsprwca03Heq6gdkaAjZ-M1z4srmKZ4GF9DGOuXwjfokf0H_XipeKvLoQq-QjaAgvE9zQkczomk0RDT9Tg-JerlJeoXnYx'
     }
 })
