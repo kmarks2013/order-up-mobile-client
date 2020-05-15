dfe94afa71abb90467afba5fae7f5be681e1e295
kmarks2013 <kmarks2013@gmail.com>
2020-05-15 14:04:16 -0400

    adds custom hook

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,9 +2,11 @@ import React, {useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
 import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
 
 const HomeScreen = () => {
     const [term, setTerm] = useState('')
+    const [searchApi, results, errorMessage] = useResults()
 
     const onTermChange = (newTerm) => {
         setTerm(newTerm)
