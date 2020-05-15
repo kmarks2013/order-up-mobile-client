be9ddf99c3fbca216b51505839d851f9ee702182
kmarks2013 <kmarks2013@gmail.com>
2020-05-15 11:27:23 -0400

    adds space to authorization

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers:{
-        Authorization: 'Bearer WYScSl9qEUwysrj6BNDJgd2BLQFwnTtnGoYsprwca03Heq6gdkaAjZ-M1z4srmKZ4GF9DGOuXwjfokf0H_XipeKvLoQq-QjaAgvE9zQkczomk0RDT9Tg-JerlJeoXnYx'
+        Authorization:
+        'Bearer WYScSl9qEUwysrj6BNDJgd2BLQFwnTtnGoYsprwca03Heq6gdkaAjZ-M1z4srmKZ4GF9DGOuXwjfokf0H_XipeKvLoQq-QjaAgvE9zQkczomk0RDT9Tg-JerlJeoXnYx'
     }
 })
