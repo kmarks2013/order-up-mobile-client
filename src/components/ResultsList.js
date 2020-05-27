372f3506c057c7ebda19744ca806a9d03696a89b
kmarks2013 <kmarks2013@gmail.com>
2020-05-27 12:35:44 -0400

    adds basic render to esults list

---
+++ src/components/ResultsList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ResultsList = () => {
+    return (
+        <View>
+            <Text>Results List</Text>
+        </View>
+    )
+}
+
+export default ResultsList
+
+const styles = StyleSheet.create({})
