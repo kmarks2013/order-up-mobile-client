9557cf7f4149f0a28f17011dcd76b8990031a16d
kmarks2013 <kmarks2013@gmail.com>
2020-05-27 12:41:18 -0400

    passes title prop

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -26,9 +26,10 @@ const HomeScreen = () => {
             <Text style={styles.homeFont}>Test</Text>
             {errorMessage ? <Text>{errorMessage}</Text>: null}
             <Text>We have found {results.length} resturants</Text>
-            <ResultsList />
-            <ResultsList />
-            <ResultsList />
+            <ResultsList title='Cost Effective'/>
+            <ResultsList title='Bit Pricier'/>
+            <ResultsList title='Big Spender'/>
+
         </View>
     )
 }
