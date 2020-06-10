aa67eab273bd494b35a45ffa2c4a16aca8b7cb71
kmarks2013 <kmarks2013@gmail.com>
2020-06-10 15:00:09 -0400

    adds broiler plate for restaruant screen

---
+++ src/screens/RestaurantScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const RestaurantScreen = () => {
+    return (
+        <View>
+            <Text> Restaruant show screen</Text>
+        </View>
+    )
+}
+
+export default RestaurantScreen
+
+const styles = StyleSheet.create({})
