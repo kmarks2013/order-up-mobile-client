24c064b4a364e50100cd1b4d0ba4f2537ff5b202
kmarks2013 <kmarks2013@gmail.com>
2020-06-10 16:24:03 -0400

    passes navigation to Restaurant Screen via navigation.getparam and runs a fetch

--- src/screens/RestaurantScreen.js
+++ src/screens/RestaurantScreen.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import yelp from '../api/yelp'
 
-const RestaurantScreen = () => {
+const RestaurantScreen = ({navigation}) => {
+    const [restaruant, setRestaurant] = useState(null)
+    const id = navigation.getParam('id')
+
+    // console.log(id)
+    console.log(restaruant)
+
+    const getRestaurant = async (id) => {
+        const response = await yelp.get(`/${id}`)
+        setRestaurant(response.data)
+    }
+
+    useEffect(() => {
+        getRestaurant(id)
+    }, [])
+
+
     return (
         <View>
             <Text> Restaruant show screen</Text>
