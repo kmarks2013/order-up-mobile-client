95804a675da2ef83849dce705f239c995d3135be
kmarks2013 <kmarks2013@gmail.com>
2020-06-10 14:57:36 -0400

    adds opening frragment tag

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import SearchBar from '../components/SearchBar'
 import useResults from '../hooks/useResults'
 import ResultsList from '../components/ResultsList'
@@ -26,7 +26,7 @@ const HomeScreen = () => {
      }
 
     return (
-        <View>
+        <>
             <SearchBar
                 term={term}
                 onTermChange={onTermChange}
@@ -34,12 +34,14 @@ const HomeScreen = () => {
             />
             {/* <Text style={styles.homeFont}>Test</Text> */}
             {errorMessage ? <Text>{errorMessage}</Text>: null}
-            <Text>We have found {results.length} resturants</Text>
-            <ResultsList results={filterResultsByPrice('$')} title='Cost Effective'/>
-            <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier'/>
-            <ResultsList results={filterResultsByPrice('$$$')} title='Big Spender'/>
+            {/* <Text>We have found {results.length} resturants</Text> */}
+            <ScrollView>
+                <ResultsList results={filterResultsByPrice('$')} title='Cost Effective'/>
+                <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier'/>
+                <ResultsList results={filterResultsByPrice('$$$')} title='Big Spender'/>
+            </ScrollView>
 
-        </View>
+        </>
     )
 }
 
