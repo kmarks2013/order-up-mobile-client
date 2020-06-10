079d587cc1729553c726ccf18011e4166f18eaa6
kmarks2013 <kmarks2013@gmail.com>
2020-06-10 12:31:03 -0400

    adds brioler plate for component

---
+++ src/components/ResturantDetail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+const ResturantDetail = () => {
+    return (
+        <View>
+            <Text>Restruant Detail </Text>
+        </View>
+    )
+}
+
+export default ResturantDetail
+
+const styles = StyleSheet.create({})
